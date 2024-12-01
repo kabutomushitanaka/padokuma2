1142c28556ade80b60ae9900c87880005980844e
takeshinakamura <takesudesu7128@gmail.com>
2024-12-01 19:09:11 +0900

    Merge branch 'main' into どうなってる？

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,45 @@ import 'package:flutter/material.dart';
 
 void main(){
 
-  final col = Column(
+  final img =
+   //画像
+      Image.asset('images/onepiece06_chopper.png',
 
-      mainAxisAlignment: MainAxisAlignment.center ,
-      crossAxisAlignment: CrossAxisAlignment.center ,
-      children: [
-        Image.asset('images/onepiece06_chopper.png',
-          width: 500, // 幅を100に指定
-          height: 500, // 高さを100に指定
-        )
+  );
+
+  // 小さいコンテナ
+  final smallCon = Container(
+    color: Colors.deepOrange, // オレンジ色
+    width: 50,
+    height: 50,
+  );
+
+  // 縦に並べるカラム
+  final col = SingleChildScrollView(
+    child:Column(
+    children: [
+      smallCon, // 小さいコンテナ
+      img, // 画像
+      smallCon, // 小さいコンテナ
     ],
+  ),
   );
 
- final row = Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  crossAxisAlignment: CrossAxisAlignment.center,
-  children: [col],
+  // 大きいコンテナ
+  final bigCon = Container(
+    color: Colors.blue, // 青色
+    width: 400,
+    height: 400,
+    alignment: Alignment.centerLeft,
+    padding: const EdgeInsets.all(20),
+    child: col, // 中にカラムを丸ごと入れる
   );
 
+  // アプリ
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: row,
+        child: bigCon, // 大きいコンテナを表示する
       ),
     ),
   );
